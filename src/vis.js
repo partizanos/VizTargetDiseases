357f9ac8cb45b511777629f1b2da067ecaae856b
Miguel Pignatelli <emepyc@gmail.com>
2016-06-09 12:29:49 +0100

    nodes + tooltips

--- src/vis.js
+++ src/vis.js
@@ -1,4 +1,5 @@
 var cttvApi = require('cttv.api');
+var tooltip = require('tnt.tooltip');
 
 var vis = function () {
     "use strict";
@@ -50,12 +51,75 @@ var vis = function () {
         api.call(url)
             .then (function (resp) {
                 console.log(resp.body.data);
+                var step = 100;
+                var stepRadians = 2 * Math.PI / step;
+                var currAngle = 0;
+                var radius = config.size/2;
+
+                for (var i=1; i<resp.body.data.length; i++) {
+                    var p = resp.body.data[i];
+                    var coords = point(circleScale(1 - p.value), currAngle);
+                    currAngle += stepRadians;
+
+                    p.x = coords[0];
+                    p.y = coords[1];
+                }
+
+                var points = svg.selectAll(".points")
+                    .data(resp.body.data, function (d) {
+                        return d.object + "-"+ d.subject;
+                    })
+                    .enter()
+                    .append("circle")
+                    .attr("cx", function (d) {
+                        return d.x;
+                    })
+                    .attr("cy", function (d) {
+                        return d.y;
+                    })
+                    .attr("r", 5)
+                    .attr("fill", "navy")
+                    .attr("transform", "translate(" + radius + "," + radius + ")")
+                    .on("click", t);
+
+                function t (d) {
+                    console.log(d);
+                     var obj = {};
+                     obj.header = d.subject;
+                     obj.rows = [];
+                     obj.rows.push({
+                         "label" : "subject",
+                         "value" : d.subject
+                     });
+                     obj.rows.push({
+                         "label" : "object",
+                         "value" : d.object
+                     });
+                     obj.rows.push({
+                         "label" : "type",
+                         "value" : d.type
+                     });
+                     obj.rows.push({
+                         "label" : "value",
+                         "value" : d.value
+                     });
+                     obj.rows.push({
+                         "label" : "score",
+                         "value" : d.value
+                     });
+                     tooltip.table()
+                         .width(180)
+                         .show_closer(true)
+                         .call (this, obj);
+                }
             });
 
     };
 
-    function update () {
-
+    function point (l, r) {
+        var x = l * Math.cos(r);
+        var y = l * Math.sin(r);
+        return [x,y];
     }
 
     // Public methods
