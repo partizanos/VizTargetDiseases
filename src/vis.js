4f8b26d926701cfd3bcb19bc7098e95b01a1ba1d
Miguel Pignatelli <emepyc@gmail.com>
2016-06-10 09:08:33 +0100

    updated labels

--- src/vis.js
+++ src/vis.js
@@ -8,7 +8,9 @@ var vis = function () {
         filter: null
     };
 
-    var points;
+    var labelSize = 100;
+
+    var points, labels, links;
 
     var api = cttvApi()
         .prefix("http://test.targetvalidation.org:8899/api/")
@@ -18,40 +20,44 @@ var vis = function () {
     var render = function (div) {
         console.log(div);
 
+        var graphSize = config.size - (labelSize*2);
+        var radius = graphSize/2;
+
         // svg
         var svg = d3.select(div)
             .append("svg")
             .attr("width", config.size)
             .attr("height", config.size);
 
-        vis = svg;
+        var graph = svg
+            .append("g")
+            .attr("transform", "translate(" + (radius + labelSize) + "," + (radius + labelSize) + ")");
 
         // draw circles
         var circleScale = d3.scale.linear()
             .domain([0, 1])
-            .range([0, config.size/2]);
+            .range([0, graphSize/2]);
 
         var circlesSize = [0, 0.2, 0.4, 0.6, 0.8, 1.0];
-        var cirlces = svg
+        var circles = graph
             .selectAll(".circle")
             .data(circlesSize)
             .enter()
             .append("circle")
-            .attr("cx", config.size/2)
-            .attr("cy", config.size/2)
+            .attr("cx", 0)
+            .attr("cy", 0)
             .attr("r", function (d) {
                 return circleScale(d);
             })
             .attr("stroke", "gray")
             .attr("fill", "none")
             .attr("class", "circle");
-            // .style("stroke-width", "1px");
 
         // get data
-        var url = api.url.diseaseRelation({
-            id: "EFO_0004591"
-        });
-        console.log(url);
+        // var url = api.url.diseaseRelation({
+        //     id: "EFO_0004591"
+        // });
+        // console.log(url);
 
         d3.json("../data/sample.json", function(error, resp) {
             var data = resp.data;
@@ -59,25 +65,22 @@ var vis = function () {
 
         // api.call(url)
         //     .then (function (resp) {
-                var stepRad = 23; // grades
-                // var step = 100;
-                // var stepRadians = 2 * Math.PI / step;
+                var stepRad = 25.2; // grades
                 var currAngle = 0;
-                var radius = config.size/2;
 
-                for (var i=1; i<data.length; i++) {
+                for (var i=0; i<data.length; i++) {
                     var p = data[i];
                     var coords = point(circleScale(1 - p.value), currAngle);
-                    // currAngle += stepRadians;
-                    currAngle += (stepRad * 2 * Math.PI / 360);
 
                     p.x = coords[0];
                     p.y = coords[1];
+                    p.angle = currAngle;
+                    currAngle += (stepRad * 2 * Math.PI / 360);
                 }
 
-                points = svg.selectAll(".points")
+                points = graph.selectAll(".points")
                     .data(data, function (d) {
-                        return d.object + "-"+ d.subject;
+                        return d.object + "-" + d.subject;
                     })
                     .enter()
                     .append("circle")
@@ -89,35 +92,88 @@ var vis = function () {
                     })
                     .attr("r", 5)
                     .attr("fill", "navy")
-                    .attr("transform", "translate(" + radius + "," + radius + ")")
-                    .on("click", t);
+                    .on("click", function (d) {
+                        console.log(d);
+                         var obj = {};
+                         obj.header = d.subject;
+                         obj.rows = [];
+                         obj.rows.push({
+                             "label" : "subject",
+                             "value" : d.subject
+                         });
+                         obj.rows.push({
+                             "label" : "object",
+                             "value" : d.object
+                         });
+                         obj.rows.push({
+                             "label" : "type",
+                             "value" : d.type
+                         });
+                         obj.rows.push({
+                             "label" : "value",
+                             "value" : d.value
+                         });
+                         tooltip.table()
+                             .width(180)
+                             .show_closer(true)
+                             .call (this, obj);
 
-                function t (d) {
-                    console.log(d);
-                     var obj = {};
-                     obj.header = d.subject;
-                     obj.rows = [];
-                     obj.rows.push({
-                         "label" : "subject",
-                         "value" : d.subject
-                     });
-                     obj.rows.push({
-                         "label" : "object",
-                         "value" : d.object
-                     });
-                     obj.rows.push({
-                         "label" : "type",
-                         "value" : d.type
-                     });
-                     obj.rows.push({
-                         "label" : "value",
-                         "value" : d.value
-                     });
-                     tooltip.table()
-                         .width(180)
-                         .show_closer(true)
-                         .call (this, obj);
-                }
+                    });
+
+
+                labels = graph.selectAll(".label")
+                    .data(data, function (d) {
+                        return d.object + "-" + d.subject;
+                    })
+                    .enter()
+                    .append("text")
+                    .attr("x", function (d) {
+                        var grades = d.angle * 360 / (2*Math.PI);
+                        return graphSize/2 * Math.cos(d.angle);
+//                        return graphSize/2 * Math.cos(grades);
+                    })
+                    .attr("y", function (d) {
+                        return graphSize/2 * Math.sin(d.angle);
+                    })
+                    .attr("class", "label")
+                    .style("font-size", "10px")
+                    .style("text-anchor", function (d) {
+                        var grades = d.angle * 360 / (2*Math.PI);
+                        if (grades % 360 > 90 && grades % 360 < 275) {
+                            return "end";
+                        }
+                        return "start";
+                    })
+                    .text(function(d) {
+                        return d.object;
+                    })
+                    .attr("transform", function (d) {
+                        var grades = d.angle * 360 / (2*Math.PI);
+                        console.log(this);
+                        console.log(d.object + " Rotate --- " + (grades % 360));
+                        // return "rotate(" + (grades % 360) + ")";
+                    });
+
+                links = graph.selectAll("links")
+                    .data(data, function (d) {
+                        return d.object + "-" + d.subject;
+                    })
+                    .enter()
+                    .append("line")
+                    .attr("x1", function (d) {
+                        return d.x;
+                    })
+                    .attr("y1", function(d) {
+                        return d.y;
+                    })
+                    .attr("x2", function(d) {
+                        return graphSize/2 * Math.cos(d.angle);
+                    })
+                    .attr("y2", function(d) {
+                        return graphSize/2 * Math.sin(d.angle);
+                    })
+                    .style("stroke", "navy");
+
             });
 
     };
