685090df9dce43976a097e203818178f06c0ec7c
Miguel Pignatelli <emepyc@gmail.com>
2016-06-12 20:21:37 +0100

    added zoom in/out functionality

--- src/vis.js
+++ src/vis.js
@@ -11,6 +11,8 @@ var vis = function () {
 
     var labelSize = 100;
 
+    var transitionSpeed = 1000;
+
     var points, labels, links;
 
     var api = cttvApi()
@@ -19,7 +21,6 @@ var vis = function () {
         .secret("2J23T20O31UyepRj7754pEA2osMOYfFK");
 
     var render = function (div) {
-        console.log(div);
 
         var graphSize = config.size - (labelSize*2);
         var radius = graphSize/2;
@@ -34,39 +35,15 @@ var vis = function () {
             .append("g")
             .attr("transform", "translate(" + (radius + labelSize) + "," + (radius + labelSize) + ")");
 
-        // draw circles / rings
-
-        var circleScale = d3.scale.linear()
-            .domain([0, 1])
-            .range([0, graphSize/2]);
-
+        // var circleScale = d3.scale.linear()
+        //     .domain([0, 1])
+        //     .range([0, graphSize/2]);
+        //
         var circleColorScale = d3.scale.linear()
             .domain([0,1])
             .range([d3.rgb(0,82,163), d3.rgb(182,221,252)]);
-            //.range(["#0000FF", "#2ECCFA"]);
 
-        var circlesSize = [0, 0.2, 0.4, 0.6, 0.8, 1.0];
-
-        var rings = graph
-            .selectAll(".ring")
-            .data(circlesSize.slice(0,circlesSize.length-1))
-            .enter()
-            .append("path")
-            .attr("class", "ring")
-            .attr("d", function (d, i) {
-                var arc = d3.svg.arc()
-                    .innerRadius(circleScale(circlesSize[i]))
-                    .outerRadius(circleScale(circlesSize[i+1]))
-                    .startAngle(0) //converting from degs to radians
-                    .endAngle(2*Math.PI); //just radians
-                return arc(d, i);
-            })
-            .attr("fill", function (d, i) {
-                return circleColorScale(circlesSize[i]);
-            })
-            .on("click", function (d) {
-                console.log(d);
-            });
+        // var circlesSize = [0, 0.2, 0.4, 0.6, 0.8, 1.0];
 
         // get data
         // var url = api.url.diseaseRelation({
@@ -76,104 +53,194 @@ var vis = function () {
 
         d3.json("../data/sample.json", function(error, resp) {
             var data = resp.data;
+            // api.call(url)
+            //     .then (function (resp) {
+            //        var data = resp.body.data;
+
             console.log(data);
+            render.update(data, updateScales(radius));
+        });
 
-        // api.call(url)
-        //     .then (function (resp) {
-                var stepRad = 25.2; // grades
-                var currAngle = 0;
+        render.update = function (data, circleScales) {
+            // Rings
+            var rings = graph
+                .selectAll(".ring")
+                .data(circleScales);
+                // .data(currCirclesSize.slice(0,currCirclesSize.length-1), function (d) {
+                //     return d;
+                // });
+            rings
+                .enter()
+                .append("path")
+                .attr("class", "ring")
+                .attr("fill", function (d) {
+                    return circleColorScale(d.domain()[0]);
+                })
+                .attr("d", function (d) {
+                    var arc = d3.svg.arc()
+                        .innerRadius(d.range()[0])
+                        .outerRadius(d.range()[1])
+                        .startAngle(0)
+                        .endAngle(2*Math.PI);
+                    return arc(d);
+                })
+                .on("click", function (d, i) {
+                    // One ring has been selected
+                    var selected = d3.select(this);
+                    if (selected.classed("zoomed")) {
+                        selected.classed("zoomed", false);
+                        render.update(data, updateScales(radius));
+                    } else {
+                        selected.classed("zoomed", true);
+                        render.update(data, updateScales(radius, d));
+                    }
+                });
+            rings
+                .transition()
+                .duration(transitionSpeed)
+                .attr("d", function (d, i) {
+                    var arc = d3.svg.arc()
+                        .innerRadius(circleScales[i].range()[0] + 0.1) // Have to add 0.1 otherwise it doesn't transition correctly
+                        .outerRadius(circleScales[i].range()[1] + 0.1) // Have to add 0.1 otherwise it doesn't transition correctly
+                        .startAngle(0)
+                        .endAngle(2*Math.PI);
+                    return arc(d, i);
+                });
 
-                for (var i=0; i<data.length; i++) {
-                    var p = data[i];
-                    var coords = point(circleScale(1 - p.value), currAngle);
+            // Calculate coords for each data point
+            var stepRad = 25.2; // grades
+            var currAngle = 0;
 
-                    p.x = coords[0];
-                    p.y = coords[1];
-                    p.angle = currAngle;
-                    currAngle += (stepRad * 2 * Math.PI / 360);
-                }
+            for (var i=0; i<data.length; i++) {
+                var p = data[i];
+                var scale = circleScales[~~((1-p.value)/0.2)];
+                var coords = point(scale(1 - p.value), currAngle);
 
+                p.x = coords[0];
+                p.y = coords[1];
+                p.angle = currAngle;
+                currAngle += (stepRad * 2 * Math.PI / 360);
+            }
 
-                links = graph.selectAll(".openTargets_d-d_overview_link")
-                    .data(data, function (d) {
-                        return d.object + "-" + d.subject;
-                    })
-                    .enter()
-                    .append("line")
-                    .attr("class", "openTargets_d-d_overview_link unselected")
-                    .attr("x1", function (d) {
-                        return d.x;
-                    })
-                    .attr("y1", function(d) {
-                        return d.y;
-                    })
-                    .attr("x2", function(d) {
-                        return graphSize/2 * Math.cos(d.angle);
-                    })
-                    .attr("y2", function(d) {
-                        return graphSize/2 * Math.sin(d.angle);
-                    })
-                    .style("stroke", "navy");
+            // Links
+            links = graph.selectAll(".openTargets_d-d_overview_link")
+                .data(data, function (d) {
+                    return d.object + "-" + d.subject;
+                });
+            links
+                .enter()
+                .append("line")
+                .style("stroke", "navy")
+                .attr("class", "openTargets_d-d_overview_link unselected");
+            links
+                .transition()
+                .duration(transitionSpeed)
+                .attr("x1", function (d) {
+                    return d.x;
+                })
+                .attr("y1", function(d) {
+                    return d.y;
+                })
+                .attr("x2", function(d) {
+                    return radius * Math.cos(d.angle);
+                })
+                .attr("y2", function(d) {
+                    return radius * Math.sin(d.angle);
+                });
 
-                points = graph.selectAll(".openTargets_d-d_overview_node")
-                    .data(data, function (d) {
-                        return d.object + "-" + d.subject;
-                    })
-                    .enter()
-                    .append("circle")
-                    .attr("class", "openTargets_d-d_overview_node selected")
-                    .attr("cx", function (d) {
-                        return d.x;
-                    })
-                    .attr("cy", function (d) {
-                        return d.y;
-                    })
-                    .attr("r", 5)
-                    .attr("fill", "navy")
-                    .on("mouseout", unselect)
-                    .on("mouseover", select)
-                    .on("click", tooltip);
+            // Nodes
+            points = graph.selectAll(".openTargets_d-d_overview_node")
+                .data(data, function (d) {
+                    return d.object + "-" + d.subject;
+                });
+            points
+                .enter()
+                .append("circle")
+                .attr("class", "openTargets_d-d_overview_node selected")
+                .on("mouseout", unselect)
+                .on("mouseover", select)
+                .on("click", tooltip);
+            points
+                .transition()
+                .duration(transitionSpeed)
+                .attr("cx", function (d) {
+                    return d.x;
+                })
+                .attr("cy", function (d) {
+                    return d.y;
+                })
+                .attr("r", 5)
+                .attr("fill", "navy");
 
+            // Labels
+            labels = graph.selectAll(".openTargets_d-d_overview_label")
+                .data(data, function (d) {
+                    return d.object + "-" + d.subject;
+                });
+            labels
+                .enter()
+                .append("g")
+                .attr("class", "openTargets_d-d_overview_label selected")
+                .attr("transform", function (d) {
+                    var grades = d.angle * 360 / (2*Math.PI);
+                    var x = graphSize/2 * Math.cos(d.angle);
+                    var y = graphSize/2 * Math.sin(d.angle);
 
-                labels = graph.selectAll(".openTargets_d-d_overview_label")
-                    .data(data, function (d) {
-                        return d.object + "-" + d.subject;
-                    })
-                    .enter()
-                    .append("g")
-                    .attr("transform", function (d) {
-                        var grades = d.angle * 360 / (2*Math.PI);
-                        var x = graphSize/2 * Math.cos(d.angle);
-                        var y = graphSize/2 * Math.sin(d.angle);
-
-                        return "translate(" + x + "," + y + ")";
-                    })
-                    .attr("class", "openTargets_d-d_overview_label selected")
-                    .append("text")
-                    .style("font-size", "10px")
-                    .style("text-anchor", function (d) {
-                        var grades = d.angle * 360 / (2*Math.PI);
-                        if (grades % 360 > 90 && grades % 360 < 275) {
-                            return "end";
-                        }
-                        return "start";
-                    })
-                    .text(function(d) {
-                        return d.object;
-                    })
-                    .attr("transform", function (d) {
-                        var grades = d.angle * 360 / (2*Math.PI);
-                        if (grades % 360 > 90 && grades % 360 < 275) {
-                            return "rotate(" + ((grades % 360) + 180) + ")";
-                        }
-                        return "rotate(" + (grades % 360) + ")";
-                    })
-                    .on("mouseover", select)
-                    .on("mouseout", unselect)
-                    .on("click", tooltip);
-            });
-
+                    return "translate(" + x + "," + y + ")";
+                })
+                .append("text")
+                .style("font-size", "10px")
+                .style("text-anchor", function (d) {
+                    var grades = d.angle * 360 / (2*Math.PI);
+                    if (grades % 360 > 90 && grades % 360 < 275) {
+                        return "end";
+                    }
+                    return "start";
+                })
+                .text(function(d) {
+                    return d.object;
+                })
+                .attr("transform", function (d) {
+                    var grades = d.angle * 360 / (2*Math.PI);
+                    if (grades % 360 > 90 && grades % 360 < 275) {
+                        return "rotate(" + ((grades % 360) + 180) + ")";
+                    }
+                    return "rotate(" + (grades % 360) + ")";
+                })
+                .on("mouseover", select)
+                .on("mouseout", unselect)
+                .on("click", tooltip);
+        };
     };
+
+    function updateScales (radius, selected) {
+        var circleScales = [
+            d3.scale.linear().domain([0.0, 0.2]).range([0,1*radius/5]).clamp(true),
+            d3.scale.linear().domain([0.2, 0.4]).range([1*radius/5, 2*radius/5]).clamp(true),
+            d3.scale.linear().domain([0.4, 0.6]).range([2*radius/5, 3*radius/5]).clamp(true),
+            d3.scale.linear().domain([0.6, 0.8]).range([3*radius/5, 4*radius/5]).clamp(true),
+            d3.scale.linear().domain([0.8, 1.0]).range([4*radius/5, 5*radius/5]).clamp(true),
+        ];
+
+        if (!selected) {
+            return circleScales;
+        }
+
+        // We are focusing on a scale
+        var newScales = [];
+        var currRad = 0;
+        for (var i=0; i<circleScales.length; i++) {
+            var scale = circleScales[i];
+            if (selected.domain()[0] == scale.domain()[0]) {
+                scale.range([currRad, radius]);
+                currRad = radius;
+            } else {
+                scale.range([currRad, currRad]);
+            }
+            newScales.push(scale);
+        }
+        return newScales;
+    }
 
     function tooltip (d) {
         console.log(d);
